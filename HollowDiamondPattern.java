549a0259bce1963c3acc958868c1f9ba9ec47bf1
Kalyani Vilas Bhawar <139538601+Kalyani-Bhawar@users.noreply.github.com>
2026-03-14 19:34:12 +0530

    Create HollowDiamondPattern.java

---
+++ HollowDiamondPattern.java
@@ -0,0 +1,39 @@
+public class HollowDiamondPattern {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows for the top half
+
+        // Top half
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars and spaces in between
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        // Bottom half
+        for (int i = n - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars and spaces in between
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
